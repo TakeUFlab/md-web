e542decfbc532b58021e83cc5273cb4fe68cffa2
banahaker <pn0818x@outlook.com>
2022-05-31 12:10:44 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-routing";
+  import Index from "./pages/index.svelte";
+  import Login from "./pages/login.svelte";
+</script>
+
+<Router url={""}>
+  <Route path="/" component={Index} />
+  <Route path="/login" component={Login} />
+</Router>
