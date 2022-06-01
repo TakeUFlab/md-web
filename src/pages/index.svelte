3ee8a65385728a4471f0bfe6a0cafd3304272aee
banahaker <pn0818x@outlook.com>
2022-06-01 09:58:23 +0800

    feat: sass

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,1 +1,16 @@
-<h1>index</h1>
+<script lang="ts">
+  import Teaching from "../assets/Teaching.svg";
+</script>
+
+<main>
+  <div class="left">
+    <h1>NewMD</h1>
+    <h2>拯救明道人的雲端課表平台</h2>
+  </div>
+  <div class="right">
+    <img src={Teaching} alt="illsu" />
+  </div>
+</main>
+
+<style>
+</style>
