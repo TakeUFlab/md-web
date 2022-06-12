4c42b33d633fb6f4779f4296a144f3d58caf8f1b
banahaker <pn0818x@outlook.com>
2022-06-12 22:21:55 +0800

    feat: header page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,8 @@
 
 <template>
   <div class="intro">
-    <h1>拯救明道學生的課表系統</h1>
+    <h1>New MD</h1>
+    <h2>拯救明道學生的課表系統</h2>
     <div class="logins">
       <button>首次登入</button>
       <button>登入</button>
@@ -14,7 +15,43 @@
 .intro {
   width: 100vw;
   padding: 50px 20px;
-  min-height: 100vh;
+  height: 800px;
+  max-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   background: url("/src/assets/bg1.jpg");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  h1 {
+    font-size: 3rem;
+    color: #003cff;
+  }
+  h2 {
+    font-size: 2.5rem;
+    color: #fff;
+    text-align: center;
+  }
+  .logins {
+    margin-top: 20px;
+    button {
+      margin: 0 10px;
+      cursor: pointer;
+      background-color: #003cff;
+      border: 1px solid #003cff;
+      color: #fff;
+      padding: 5px 10px;
+      border-radius: 5px;
+      font-size: 1.2rem;
+      font-weight: 300;
+      transition: all 0.2s ease-in-out;
+      &:hover {
+        background-color: transparent;
+        color: #003cff;
+      }
+    }
+  }
 }
 </style>
